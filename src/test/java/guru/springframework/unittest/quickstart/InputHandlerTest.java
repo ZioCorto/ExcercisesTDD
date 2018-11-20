181dd19f3bb2e5350e226428291cb976fb7cc3ee
ZioCorto <ZioCorto@users.noreply.github.com>
2018-11-20 11:11:37 +0100

    Refactor of InputHandler class.

--- src/test/java/guru/springframework/unittest/quickstart/InputHandlerTest.java
+++ src/test/java/guru/springframework/unittest/quickstart/InputHandlerTest.java
@@ -3,21 +3,23 @@ package guru.springframework.unittest.quickstart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 class InputHandlerTest {
-    InputHandler handler = new InputHandler();
+    private InputHandler handler = new InputHandler();
 
-    String filepath = "files/ExampleGoodInput.txt";
-    List<QuadraticCoefficients> inputCoefficients = new ArrayList<>();
+    private List<QuadraticCoefficients> inputCoefficients = new ArrayList<>();
 
 
     @Test
-    void parseInputFileTest() {
+    void parseInputFileTest() throws IOException {
         inputCoefficients.add(new QuadraticCoefficients(2, 3, 4));
         inputCoefficients.add(new QuadraticCoefficients(3, 5, 1));
         inputCoefficients.add(new QuadraticCoefficients(1, -4, 4));
+
+        String filepath = "files/ExampleGoodInput.txt";
 
         Assertions.assertEquals(handler.parseInputFile(filepath), inputCoefficients);
     }
