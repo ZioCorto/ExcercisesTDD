897dc644d46c5bdc13f28490ef649e235cb0d4b8
ZioCorto <ZioCorto@users.noreply.github.com>
2018-11-20 16:59:12 +0100

    Test of exceptions that should fail, but don't...

--- src/test/java/guru/springframework/unittest/quickstart/InputHandlerTest.java
+++ src/test/java/guru/springframework/unittest/quickstart/InputHandlerTest.java
@@ -8,19 +8,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 class InputHandlerTest {
-    private InputHandler handler = new InputHandler();
-
-    private List<QuadraticCoefficients> inputCoefficients = new ArrayList<>();
 
 
     @Test
     void parseInputFileTest() throws IOException {
+        InputHandler handler = new InputHandler();
+        List<QuadraticCoefficients> inputCoefficients = new ArrayList<>();
         inputCoefficients.add(new QuadraticCoefficients(2, 3, 4));
         inputCoefficients.add(new QuadraticCoefficients(3, 5, 1));
         inputCoefficients.add(new QuadraticCoefficients(1, -4, 4));
 
-        String filepath = "files/ExampleGoodInput.txt";
-
+        String filepath = "files/GoodInput.txt";
         Assertions.assertEquals(handler.parseInputFile(filepath), inputCoefficients);
     }
+
+    //FIXME this test should fail because the exception message is different...
+    @Test
+    void declaredBiggerThanProvidedTest() {
+        InputHandler handler = new InputHandler();
+
+        String filepath = "files/DeclaredBiggerThanProvided.txt";
+        Assertions.assertThrows(ArithmeticException.class, () -> handler.parseInputFile(filepath), "WhyIsNotFailing?");
+    }
+
+    //FIXME this test should fail because the exception message is different...
+    @Test
+    void declaredSmallerThanProvidedTest() {
+        InputHandler handler = new InputHandler();
+
+        String filepath = "files/DeclaredSmallerThanProvided.txt";
+        Assertions.assertThrows(ArithmeticException.class, () -> handler.parseInputFile(filepath), "WhyIsNotFailing?");
+    }
+
+    //FIXME this test should fail because the exception message is different...
+    @Test
+    void tooFewTestCasesTest() {
+        InputHandler handler = new InputHandler();
+
+        String filepath = "files/TooFewTestCases.txt";
+        Assertions.assertThrows(ArithmeticException.class, () -> handler.parseInputFile(filepath), "WhyIsNotFailing?");
+    }
+
+    //TODO add many more tests for each coefficient, malformed input, etc.
 }
