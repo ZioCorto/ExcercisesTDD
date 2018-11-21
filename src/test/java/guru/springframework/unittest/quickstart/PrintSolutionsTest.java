4540dcdd68a7e2a03b4b8fb9c11cb16227155ca1
ZioCorto <ziocorto@user.noreply.github.com>
2018-11-21 10:03:37 +0100

    More tests.

--- src/test/java/guru/springframework/unittest/quickstart/PrintSolutionsTest.java
+++ src/test/java/guru/springframework/unittest/quickstart/PrintSolutionsTest.java
@@ -25,7 +25,7 @@ class PrintSolutionsTest {
     }
 
     @Test
-    void printTest() {
+    void printComplexTest() {
         List<QuadraticCoefficients> coefficients = new ArrayList<>();
         coefficients.add(new QuadraticCoefficients(2, 3, 4));
 
@@ -33,5 +33,37 @@ class PrintSolutionsTest {
         printer.print();
 
         Assertions.assertEquals("COMPLEX" + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    void printRealTest() {
+        List<QuadraticCoefficients> coefficients = new ArrayList<>();
+        coefficients.add(new QuadraticCoefficients(1, -4, -21));
+
+        PrintSolutions printer = new PrintSolutions(coefficients);
+        printer.print();
+
+        Assertions.assertEquals("-3.0 7.0" + System.lineSeparator(), outContent.toString());
+    }
+
+    @Test
+    void printMixedTest() {
+        List<QuadraticCoefficients> coefficients = new ArrayList<>();
+        coefficients.add(new QuadraticCoefficients(2, 3, 4));
+        coefficients.add(new QuadraticCoefficients(1, -4, -21));
+        coefficients.add(new QuadraticCoefficients(2, 1, 2));
+        coefficients.add(new QuadraticCoefficients(3, 3, 3));
+        coefficients.add(new QuadraticCoefficients(1, -3, 2));
+        coefficients.add(new QuadraticCoefficients(4, 12, 9));
+
+        PrintSolutions printer = new PrintSolutions(coefficients);
+        printer.print();
+
+        Assertions.assertEquals("COMPLEX" + System.lineSeparator() +
+                "-3.0 7.0" + System.lineSeparator() +
+                "COMPLEX" + System.lineSeparator() +
+                "COMPLEX" + System.lineSeparator() +
+                "1.0 2.0" + System.lineSeparator() +
+                "-1.5 -1.5" + System.lineSeparator(), outContent.toString());
     }
 }
