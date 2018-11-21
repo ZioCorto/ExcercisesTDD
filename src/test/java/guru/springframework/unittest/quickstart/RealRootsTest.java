da51be189a670f0297f78225c72a4b274c5f866d
ZioCorto <ziocorto@users.noreply.github.com>
2018-11-21 09:58:39 +0100

    Tests added.

--- src/test/java/guru/springframework/unittest/quickstart/RealRootsTest.java
+++ src/test/java/guru/springframework/unittest/quickstart/RealRootsTest.java
@@ -3,10 +3,32 @@ package guru.springframework.unittest.quickstart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class RealRootsTest {
+class RealRootsTest {
     @Test
     void testOverrideEquals() {
-        Assertions.assertEquals(new RealRoots(1.0, 2.0),
-                new RealRoots(1.0, 2.0));
+        RealRoots example = new RealRoots(-1.0, -1.5);
+        RealRoots same_example_values = new RealRoots(-1.0, -1.5);
+
+        Assertions.assertEquals(example, example);
+        Assertions.assertEquals(example, same_example_values);
+
+        Assertions.assertNotEquals(null, example);
+        Assertions.assertNotEquals("FooString", example);
+    }
+
+    @Test
+    void testGetSmallerRoot() {
+        RealRoots example = new RealRoots(3.4, 4.4);
+
+        Assertions.assertEquals((Double) 3.4, example.getSmallerRoot());
+        Assertions.assertFalse(4.4 == example.getSmallerRoot());
+    }
+
+    @Test
+    void testGetBiggerRoot() {
+        RealRoots example = new RealRoots(3.1, 6.1);
+
+        Assertions.assertEquals((Double) 6.1, example.getBiggerRoot());
+        Assertions.assertFalse(6.1 == example.getSmallerRoot());
     }
 }
