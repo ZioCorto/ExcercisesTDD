3c98930d4a4913a7dc8c14b830a0e475fb4fca4a
ZioCorto <ziocorto@user.noreply.github.com>
2018-11-23 12:36:44 +0100

    Example of run.

--- src/main/java/excercises/Main.java
+++ src/main/java/excercises/Main.java
@@ -1,7 +1,19 @@
 package excercises;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        String filepath = "files/GoodInput.txt";
+        InputHandler handler = new InputHandler();
+        try {
+            List<QuadraticCoefficients> coefficients = handler.parseInputFile(filepath);
+            PrintSolutions printer = new PrintSolutions(coefficients);
+            printer.print();
+        } catch (IOException e) {
+            System.out.println("Something went wrong reading the input file...");
+        }
     }
 }
