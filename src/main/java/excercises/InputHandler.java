af6d03ce0bd3199b2c7c7b52ca198751025157f5
ZioCorto <ziocorto@user.noreply.github.com>
2018-11-23 14:37:24 +0100

    Refactor.

--- src/main/java/excercises/InputHandler.java
+++ src/main/java/excercises/InputHandler.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 class InputHandler {
+    private static final String NOT_VALID_TEST_CASES = "Number of test cases not allowed!";
+    private static final String NOT_VALID_A = "Coefficient A not allowed!";
+    private static final String NOT_VALID_B = "Coefficient B not allowed!";
+    private static final String NOT_VALID_C = "Coefficient C not allowed!";
+    private static final String MISMATCH_DECLARED_PROVIDED =
+            "Number of declared test cases and provided test cases does not match!";
     private List<QuadraticCoefficients> coefficients;
     private InputValidator validator;
 
@@ -26,11 +32,12 @@ class InputHandler {
         }
     }
 
-    private List<QuadraticCoefficients> matchDeclaredProvided(Integer declared) {
-        if (coefficients.size() == declared) {
-            return coefficients;
+    private Integer builtTestCases(String testCasesStr) {
+        Integer testCases = Integer.parseInt(testCasesStr);
+        if (validator.isValidTestCases(testCases)) {
+            return testCases;
         } else {
-            throw new NotValidInputException("Number of declared test cases and provided test cases does not match!");
+            throw new NotValidInputException(NOT_VALID_TEST_CASES);
         }
     }
 
@@ -42,21 +49,12 @@ class InputHandler {
         coefficients.add(new QuadraticCoefficients(a, b, c));
     }
 
-    private Integer builtTestCases(String testCasesStr) {
-        Integer testCases = Integer.parseInt(testCasesStr);
-        if (validator.isValidTestCases(testCases)) {
-            return testCases;
-        } else {
-            throw new NotValidInputException("Number of test cases not allowed!");
-        }
-    }
-
     private Integer buildA(String aStr) {
         Integer a = Integer.parseInt(aStr);
         if (validator.isValidA(a)) {
             return a;
         } else {
-            throw new NotValidInputException("Coefficient A not allowed!");
+            throw new NotValidInputException(NOT_VALID_A);
         }
     }
 
@@ -65,7 +63,7 @@ class InputHandler {
         if (validator.isValidB(b)) {
             return b;
         } else {
-            throw new NotValidInputException("Coefficient B not allowed!");
+            throw new NotValidInputException(NOT_VALID_B);
         }
     }
 
@@ -74,7 +72,15 @@ class InputHandler {
         if (validator.isValidC(c)) {
             return c;
         } else {
-            throw new NotValidInputException("Coefficient C not allowed!");
+            throw new NotValidInputException(NOT_VALID_C);
+        }
+    }
+
+    private List<QuadraticCoefficients> matchDeclaredProvided(Integer declared) {
+        if (coefficients.size() == declared) {
+            return coefficients;
+        } else {
+            throw new NotValidInputException(MISMATCH_DECLARED_PROVIDED);
         }
     }
 }
